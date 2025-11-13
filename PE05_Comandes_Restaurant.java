c19869273d0178a34652c98c4a734bbd7ae8b486
Miguel Sanchez Cuadros <Mikikanet@gmail.com>
2025-11-13 16:30:18 +0100

    Creacio del menu i definicio del nom dels metodes

--- PE05_Comandes_Restaurant.java
+++ PE05_Comandes_Restaurant.java
@@ -1,5 +1,108 @@
 package Practica5;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+import Practica5.PE05_Comandes_Restaurant;
 
 public class PE05_Comandes_Restaurant {
     
+    //Variables globals
+    Scanner e = new Scanner(System.in);
+    String comanda = "";
+    String nomClient = "";
+    double totalSenseIVA = 0.0;
+    final double iva= 0.10;
+
+     public static void main(String[] args) {
+        PE05_Comandes_Restaurant p = new PE05_Comandes_Restaurant();
+        p.inici();
+    }
+    public void inici() {
+        int opcio = 0;
+        do {
+            try {
+                System.out.println("\n_______________________________________");
+                System.out.println("===== Gestió comandes restaurant ======");
+                System.out.println("_______________________________________");
+                System.out.println("1. Crear nova comanda");
+                System.out.println("2. Actualitzar comanda anterior");
+                System.out.println("3. Visualitzar últim tiquet");
+                System.out.println("4. Sortir");
+                System.out.println("Tria una opció: ");
+                opcio = e.nextInt();
+
+                switch (opcio) {
+                    case 1:
+                        novacomanda();
+                    break;
+                    case 2:
+                        actualitzarComanda();
+                    break;
+                    case 3:
+                        mostrarTiquet();
+                    break;
+                    case 4:
+                        System.out.println("Sortint del programa...");
+                    break;
+                    default:
+                        System.out.println("Opció no valida.")
+                }
+
+            } catch (InputMismatchException e) {
+               System.out.println("Error: introdueix un número vàlid!");
+               e.nextLine();
+            }
+        } while (opcio != 4);
+
+        e.close();
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
